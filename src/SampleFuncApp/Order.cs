10e23882a1b8c9346ecc19538f3bddba5fe2f034
Bora Aydın <boraaaydin@gmail.com>
2019-05-16 22:18:45 +0300

    4-5 Webhook Trigger and Queue output binding

---
+++ src/SampleFuncApp/Order.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleFuncApp
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public string Email { get; set; }
+        public int ProductId { get; set; }
+    }
+}
